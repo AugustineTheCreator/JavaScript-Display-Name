0f5f3fbdd982405b872c292a0edc1b6ccefff444
CHUKWUNTA AUGUSTINE <chukwuntaaugustine@gmail.com>
2022-06-22 23:09:33 +0100

    initial
    first JS

---
+++ display.js
@@ -0,0 +1,3 @@
+document.getElementById("my-name").innerHTML = "CHIDIEBERE AUGUSTINE CHUKWUNTA";
+document.getElementById("my-height").innerHTML = "HEIGHT: 6ft";
+document.getElementById("my-country").innerHTML = "COUNTRY: NIGERIA";
